a8cded375f991cdb5f76e2b7c25141f4c91bad5b
mikee-herrera <mikaela_herrera@dlsu.edu.ph>
2025-11-09 01:18:48 +0800

    edited package and reservations schemas

--- models/Reservation.js
+++ models/Reservation.js
@@ -1,21 +1,107 @@
 // models/Reservation.js
 const mongoose = require('mongoose');
 
+// Schema for packages
 const PackageSchema = new mongoose.Schema({
-  meal: { type: String, default: 'Standard' },
-  seat: { type: String, required: true },
-  baggage: { type: String, default: 'No Extra Baggage' }
+  meal: { 
+    type: String, 
+    enum: ['Standard (included)', 'Vegetarian', 'Vegan', 'Halal', 'Kosher'],
+    default: 'Standard (included)' 
+  },
+  mealCost : {
+    type: Number,
+    default: 0
+  },
+  seat: { 
+    type: String, 
+    required: true,
+    trim: true
+  },
+  baggage: { //will fix this when i figure out how baggage will work T_T
+    type: String, 
+    default: 'No Extra Baggage' 
+  },
+  baggageWeight: {
+    type: Number,
+    default: 0
+  }
 });
 
 const ReservationSchema = new mongoose.Schema({
-  bookingId: { type: String, required: true, unique: true },
-  passengerName: { type: String, required: true },
-  passengerEmail: { type: String, required: true },
+  bookingId: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  passengerName: { 
+    type: String, 
+    required: true 
+  },
+  passengerEmail: { 
+    type: String, 
+    required: true 
+  },
   passport: String,
-  flight: { type: mongoose.Schema.Types.ObjectId, ref: 'Flight', required: true },
+  tripType: {
+    type: String,
+    enum: ['Round-trip', 'One-way', 'Multi-city'],
+    default: 'Round-trip'
+  },
+  travelClass: {
+    type: String,
+    enum: ['Economy', 'Premium Economy', 'Business', 'First Class'],
+    default: 'Economy'
+  },
+  adults: { //passenger counts
+    type: Number, 
+    default: 1
+  },
+  children: {
+    type: Number,
+    default: 0
+  },
+  infants: {
+    type: Number,
+    default: 0
+  },
+  passengerCost: { // pricing stuff
+    type: Number, 
+    required: true
+  },
+  tripTypeCost: {
+    type: Number,
+    default: 0
+  },
+  travelClassCost: {
+    type: Number,
+    default: 0
+  },
+  mealCost: {
+    type: Number,
+    default: 0
+  },
+  baggageCost: {
+    type: Number,
+    default: 0
+  },
+  totalPrice: {
+    type: Number,
+    required: true
+  },
+  flight: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Flight', 
+    required: true 
+  },
   package: PackageSchema,
-  status: { type: String, enum: ['Pending', 'Confirmed', 'Cancelled'], default: 'Pending' },
-  createdAt: { type: Date, default: Date.now }
+  status: { 
+    type: String, 
+    enum: ['Pending', 'Confirmed', 'Cancelled'], 
+    default: 'Pending' },
+  createdAt: { 
+    type: Date, 
+    default: Date.now 
+  }
 });
 
 module.exports = mongoose.model('Reservation', ReservationSchema);
