e93abce0ca4fe2c0fccb9b4fbc9caed1fe64561f
pitowalosian <sian_obregon@dlsu.edu.ph>
2025-11-21 10:10:40 +0800

    Implement password hashing and comparison methods
    Added password hashing and comparison methods to User model.

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
     firstname: { type: String, required: true },
@@ -7,5 +8,23 @@ const UserSchema = new mongoose.Schema({
     password:  { type: String, required: true },
     isAdmin:   { type: Boolean, default: false }
 });
+
+// 🔐 Pre-save hook to hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next(); // Only hash if password is new or modified
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// ➕ Add method to compare password
+userSchema.methods.comparePassword = function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
 
 module.exports = mongoose.model("User", UserSchema);
